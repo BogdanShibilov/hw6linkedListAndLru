8fd5af8200d50a7636fb60cf896be55f5b1ec98f
Bogdan <bogdanshibilov@gmail.com>
2024-06-05 16:07:19 +0500

    Extracted swapping items from PushBack to pushBack func Added Print func Front swapped with Back so Front's Next is not nil, but Prev is nil

--- linkedlist.go
+++ linkedlist.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -52,8 +54,8 @@ func (l *LinkedList) pushFront(newItem *ListItem) {
 		l.frontItem = newItem
 		l.backItem = newItem
 	} else {
-		l.frontItem.Next = newItem
-		newItem.Prev = l.frontItem
+		l.frontItem.Prev = newItem
+		newItem.Next = l.frontItem
 		l.frontItem = newItem
 	}
 }
@@ -61,17 +63,21 @@ func (l *LinkedList) pushFront(newItem *ListItem) {
 func (l *LinkedList) PushBack(value any) *ListItem {
 	newItem := &ListItem{Value: value}
 
+	l.pushBack(newItem)
+
+	l.length++
+	return newItem
+}
+
+func (l *LinkedList) pushBack(newItem *ListItem) {
 	if l.length == 0 {
 		l.frontItem = newItem
 		l.backItem = newItem
 	} else {
-		l.backItem.Prev = newItem
-		newItem.Next = l.backItem
+		l.backItem.Next = newItem
+		newItem.Prev = l.backItem
 		l.backItem = newItem
 	}
-
-	l.length++
-	return newItem
 }
 
 func (l *LinkedList) Remove(i *ListItem) {
@@ -79,11 +85,11 @@ func (l *LinkedList) Remove(i *ListItem) {
 		l.Clear()
 		return
 	} else if l.frontItem == i {
-		l.frontItem.Prev.Next = nil
-		l.frontItem = l.frontItem.Prev
+		l.frontItem.Next.Prev = nil
+		l.frontItem = l.frontItem.Next
 	} else if l.backItem == i {
-		l.backItem.Next.Prev = nil
-		l.backItem = l.backItem.Next
+		l.backItem.Prev.Next = nil
+		l.backItem = l.backItem.Prev
 	} else {
 		i.Prev.Next = i.Next
 		i.Next.Prev = i.Prev
@@ -94,6 +100,7 @@ func (l *LinkedList) Remove(i *ListItem) {
 
 func (l *LinkedList) MoveToFront(i *ListItem) {
 	l.Remove(i)
+	i.Prev = nil
 	l.length++
 	l.pushFront(i)
 }
@@ -102,4 +109,12 @@ func (l *LinkedList) Clear() {
 	l.frontItem = nil
 	l.backItem = nil
 	l.length = 0
+}
+
+func (l *LinkedList) Print() {
+	fmt.Print("Front ")
+	for item := l.Back(); item != nil; item = item.Prev {
+		fmt.Printf("%v ", item.Value)
+	}
+	fmt.Print(" Back\n")
 }
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
 func main() {
+	l1 := NewList()
+
+	l1.PushFront(10) // [10]
+	l1.PushFront(20) // [20 10]
+	l1.PushFront(30) // [30 20 10]
+
+	l1.Print() // Вывод должен быть 30 20 10
+
+	l2 := NewList()
+
+	l2.PushBack(10) // [10]
+	l2.PushBack(20) // [10 20]
+	l2.PushBack(30) // [10 20 30]
+
+	l2.Print() // Вывод должен быть 10 20 30
 
 }
